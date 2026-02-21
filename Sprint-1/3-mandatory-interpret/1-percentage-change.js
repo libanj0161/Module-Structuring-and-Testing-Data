f4c8a01d6bf4e4c8a901d39c9550c8594425f0a5
Liban Jama <jliban756@gmail.com>
2026-02-21 15:56:48 +0000

    completed 1-percentage-change.js

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -12,11 +12,12 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
-
+// there are three function calls on lines 4, 5, and 8.
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
-
+// the error is on line 5. There needs to be a comma after the quotation marks to make it a correct Javascript syntax. 
 // c) Identify all the lines that are variable reassignment statements
-
+// lines 4 and 5.
 // d) Identify all the lines that are variable declarations
-
+// lines 1, 2, 6, and 7.
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+// it is removing the commas so that it has a numeric value as opposed to a formatted string. This allows for calculations by making the string convertible to numbers. The replaceAll() removes all commas, and the Number() makes the result a numeric value.  
