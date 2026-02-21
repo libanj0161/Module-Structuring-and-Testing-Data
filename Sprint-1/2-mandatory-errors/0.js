a2327fa95340d9db87f90db017fd0ed5e41a2d82
Liban Jama <jliban756@gmail.com>
2026-02-21 13:49:47 +0000

    completed 0.js

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,4 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+//This is just an instruction for the first activity - but it is just for human consumption
+//We don't want the computer to run these 2 lines - how can we solve this problem?
+
+//I put these two lines in a comment.
