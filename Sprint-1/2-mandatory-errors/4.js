5e2f25addf30796942e181afef3db76d833adf09
Liban Jama <jliban756@gmail.com>
2026-02-21 15:21:18 +0000

    completed 4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,4 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const twelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53";
+
+// i changed the variables so that they start with a letter and not a number as it was throwing an error.
