1f388153a0c2a8b2f2582bea2fe1eb64de24044a
Liban Jama <jliban756@gmail.com>
2026-02-21 14:50:18 +0000

    completed 2.js

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,7 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+// it showed error as const cannot be declared after console.log. I put the const before the console.log.
