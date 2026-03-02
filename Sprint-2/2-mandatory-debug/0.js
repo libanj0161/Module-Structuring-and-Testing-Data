a858eb9cd1def42bbb47ef3b0862f1ca6b4a457e
Liban Jama <jliban756@gmail.com>
2026-03-02 11:21:19 +0000

    fixed 2-mandatory-debug

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,7 +1,9 @@
 // Predict and explain first...
 
 // =============> write your prediction here
-// the function will not return anything.
+
+// the function will not return anything, as there is no return.
+
 function multiply(a, b) {
   console.log(a * b);
 }
@@ -10,7 +12,7 @@ console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
 
-// declared a variable called result and stored a*b.
+// declared a variable called result and stored a*b, then return result in the function.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -2,19 +2,21 @@
 //  =============> write your prediction here
 // This appears correct. The function has been defined and called sum, a is 10 and b is 32 so we should get 42.
 function sum(a, b) {
-  return;
-a + b;
+  return; 
+  a + b
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
-// The sum of 10 and 32 is undefined. On line five the function has been exited because of the ; this because the function doesn't return anything.
+
+// The sum of 10 and 32 is undefined. On line five the function has been exited early because of the return;
+// a + b need to be on the same line as the return otherwise you'd get "unreachable code detected" error.
+
 // Finally, correct the code to fix the problem
+
 //  =============> write your new code here
 
   function sum(a, b) {
   return a + b;
 }
-
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
