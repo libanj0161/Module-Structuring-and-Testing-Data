b48a37b13681114d4ee4adb3dcf5db613cfe108b
Liban Jama <jliban756@gmail.com>
2026-03-06 19:20:27 +0000

    Completed get-ordinal-number

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,18 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  const lastdigit = num % 10;
+  if (lastdigit === 1) {
+    return `${num}st`;
+  }
+
+  if (lastdigit === 2) {
+    return `${num}nd`;
+  }
+
+  if (lastdigit === 3) {
+    return `${num}rd`;
+  } else {
+    return `${num}th`;
+  }
 }
 
 module.exports = getOrdinalNumber;
